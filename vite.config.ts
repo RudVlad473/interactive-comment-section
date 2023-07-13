fcb11f56d4359463fcf00c75e351d768524ebaf1
Rudenko Vladimir <72340706+RudVlad473@users.noreply.github.com>
2023-07-13 12:16:28 +0300

    init variables

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
 import stylelintPlugin from "vite-plugin-stylelint"
+import tsconfigPaths from "vite-tsconfig-paths"
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -8,6 +9,9 @@ export default defineConfig({
     react(),
     stylelintPlugin({
       fix: true,
+    }),
+    tsconfigPaths({
+      
     }),
   ],
 })
